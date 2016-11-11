9d742c6368f506bae326c675fe16d7c02e5a1261
Md. Shorifuzzaman <shohan.jess@gmail.com>
2016-11-12 01:56:20 +0600

    Edited to distinguish clients using Color

--- src/rpp/ClientHandler.java
+++ src/rpp/ClientHandler.java
@@ -9,9 +9,14 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.InterruptedIOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.PrintStream;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.text.BadLocationException;
 
 /**
  *
@@ -23,13 +28,14 @@ public class ClientHandler extends Thread {
     private Socket clientSocket = null;
     private final ClientHandler[] t;
     private int maxClients;
-    private String clientName;
-    private BufferedReader br = null;
-    private PrintStream outputStream = null;
-
-    private static ArrayList<String> clients = new ArrayList<String>();
-
-    private static String lastMessage = "";
+   	private String clientName;
+	private ObjectInputStream br = null;
+	private ObjectOutputStream outputStream = null;
+        String monitor="s";
+          message mes = null;  
+	private static ArrayList<message> clients = new ArrayList<message>();
+	
+	private static message lastMessage = null;
 
     public ClientHandler(Socket s, ClientHandler[] t) {
         this.clientSocket = s;
@@ -39,102 +45,108 @@ public class ClientHandler extends Thread {
 
     public void run() {
         try {
-			//sets a timeout
-            //this.clientSocket.setSoTimeout(5000);
-
-            //Creates the input and output streams
-            br = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            outputStream = new PrintStream(clientSocket.getOutputStream());
-
-            String username = readUntilNull(br);
-
-            if (!clients.contains(username)) {
-                clients.add(username); //Adds user to array list
-                this.clientName = username;
-                sendOnlineUsers(); //Sends string containing online users to every client
-                if (clients.size() >= 2) {
-                    System.out.println(lastMessage);
-                    sendMessageToAll(lastMessage);
-                }
-            } else {
-                this.outputStream.print("/server Sorry! You cannot connect due to an already existing username." + 0);
-                this.outputStream.print("/server Please restart your client and try another username." + 0); //write to stream
-                this.outputStream.flush();
-                disconnect();
-                clientSocket.close();
-                outputStream.close();
-                br.close();
-                return;
-            }
-
+                br = new ObjectInputStream(this.clientSocket.getInputStream());
+                            message m = null;
+                             m = (message)br.readObject();
+                outputStream = new ObjectOutputStream(clientSocket.getOutputStream());
+                String username = m.username;
+            if(!clients.contains(username)) {
+				clients.add(m); //Adds user to array list
+				this.clientName = username;
+                             try {
+                                 sendOnlineUsers(); //Sends string containing online users to every client
+                             } catch (IOException ex) {
+                                 Logger.getLogger(ClientHandler.class.getName()).log(Level.SEVERE, null, ex);
+                             }
+				if(clients.size() >= 2){
+					System.out.println(lastMessage);
+                                    try {
+                                        sendMessageToAll(lastMessage);
+                                    } catch (IOException ex) {
+                                        Logger.getLogger(ClientHandler.class.getName()).log(Level.SEVERE, null, ex);
+                                    }
+				}
+			} 
+			else {
+                             try {
+                                 this.outputStream.writeObject(new message("/server Sorry! You cannot connect due to an already existing username." + 0));
+                             } catch (IOException ex) {
+                                 Logger.getLogger(ClientHandler.class.getName()).log(Level.SEVERE, null, ex);
+                             }
+                             try {
+                                 this.outputStream.writeObject(new message("/server Please restart your client and try another username." + 0)); //write to stream
+                             } catch (IOException ex) {
+                                 Logger.getLogger(ClientHandler.class.getName()).log(Level.SEVERE, null, ex);
+                             }
+                             try {
+                                 this.outputStream.flush();
+                             } catch (IOException ex) {
+                                 Logger.getLogger(ClientHandler.class.getName()).log(Level.SEVERE, null, ex);
+                             }
+				disconnect();
+                             try {
+                                 clientSocket.close();
+                             } catch (IOException ex) {
+                                 Logger.getLogger(ClientHandler.class.getName()).log(Level.SEVERE, null, ex);
+                             }
+                             try {
+                                 outputStream.close();
+                             } catch (IOException ex) {
+                                 Logger.getLogger(ClientHandler.class.getName()).log(Level.SEVERE, null, ex);
+                             }
+                             try {
+                                 br.close();
+                             } catch (IOException ex) {
+                                 Logger.getLogger(ClientHandler.class.getName()).log(Level.SEVERE, null, ex);
+                             }
+				return;
+			}
             //Start of server-client communication
             mainLoop:
             while (true) {
-                br = new BufferedReader(new InputStreamReader(this.clientSocket.getInputStream()));
-                String message = readUntilNull(br);
-                sendMessage(message);
-                lastMessage = message;
+                mes = (message)br.readObject();
 
-                if (message.startsWith("/disconnect")) {
-                    for (int i = 0; i < clients.size(); i++) { //Loops through client array
-                        if (clients.get(i).equals(username)) { //if it finds matching client username
-                            clients.remove(i); //remove client from list
-                            sendOnlineUsers(); //Sends string containing online users to every client
-                            disconnect();
-                            clientSocket.close();
-                            outputStream.close();
-                            br.close();
-                            break mainLoop;
-                        }
-                    }
-                }
+                                if(mes!=null){
+                                    sendMessage(mes);
+				lastMessage = mes;
+				}
+                                
+                              
+                                
+				if(mes.type==null && mes.message.startsWith("/disconnect")){
+					for(int i = 0; i < clients.size(); i++){ //Loops through client array
+						if(clients.get(i).equals(username)){ //if it finds matching client username
+							clients.remove(i); //remove client from list
+							sendOnlineUsers(); //Sends string containing online users to every client
+							disconnect();
+							clientSocket.close();
+							outputStream.close();
+							br.close();
+							break mainLoop;
+						}
+					}
+				}
+                                
             }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    //get the username from input
-    public static String readUntilNull(BufferedReader reader) {
-        StringBuilder sb = new StringBuilder();
-        String message = "";
-        try {
-            int ch;
-            while ((ch = reader.read()) != -1) {
-                if (ch == 0) {
-                    message = sb.toString();
-                    break;
-                } else if (ch == 10) {
-                    sb.append("\n");
-                } else {
-                    sb.append((char) ch);
-                }
-            }
-        } catch (InterruptedIOException ex) {
-            System.err.println("timeout!");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        System.out.println("messgare username : " + message);
-        return message;
-    }
-
-    private void sendOnlineUsers() {
-        String userlist = "|"; //Userlist structure: "|user1|user2|user3|user4|..etc"
-        for (int j = 0; j < clients.size(); j++) {
-            userlist += clients.get(j) + "|";
+        } catch (IOException ex) {
+            Logger.getLogger(ClientHandler.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ClientHandler.class.getName()).log(Level.SEVERE, null, ex);
         }
 
-        synchronized (this) {
-            for (int i = 0; i < maxClients; i++) {
-                if (t[i] != null && t[i].clientName != null) {
-                    t[i].outputStream.print(userlist);
-                    t[i].outputStream.write(0x00);
-                    t[i].outputStream.flush();
-                }
-            }
-        }
+			
+		
+	}
+
+    private void sendOnlineUsers() throws IOException {
+      synchronized(this){
+			for(int i = 0; i < maxClients; i++){
+				if(t[i] != null && t[i].clientName != null){
+					t[i].outputStream.writeObject(new message("userlist",clients));
+					t[i].outputStream.flush();
+				}
+			}
+		}
     }
 
     private void disconnect() {
@@ -147,38 +159,39 @@ public class ClientHandler extends Thread {
         }
     }
 
-    public void sendMessage(String message) {
+public void sendMessage(message message) throws IOException{
         synchronized (this) {
-            for (int i = 0; i < maxClients; i++) { //loops through every client thread
-                if (t[i] != null && t[i] != this && t[i].clientName != null) { //finds the other clients (except the current client)
-                    t[i].outputStream.print(message);
-                    t[i].outputStream.write(0x00);
-                    t[i].outputStream.flush();
-                }
-            }
+            for(int i = 0; i < maxClients; i++){ //loops through every client thread
+				if(t[i] != null && t[i] != this && t[i].clientName != null){ //finds the other clients (except the current client)
+					t[i].outputStream.writeObject(message);
+					//t[i].outputStream.write(0x00);
+					t[i].outputStream.flush();
+				}
+			}
         }
     }
 
-    public void sendMessageToAll(String message) {
-        synchronized (this) {
-            for (int i = 0; i < maxClients; i++) { //loops through every client thread
-                if (t[i] != null && t[i].clientName != null) { //finds all valid clients
-                    t[i].outputStream.print(message);
-                    t[i].outputStream.write(0x00);
-                    t[i].outputStream.flush();
-                }
-            }
-        }
-    }
-
-    public void writeToStream(String message) {
-        outputStream.print(message);
-        outputStream.write(0x00);
-        outputStream.flush();
-    }
-
-    public String getClientName() {
-        return clientName;
-    }
+	public void sendMessageToAll(message message) throws IOException{
+		synchronized(this){
+			for(int i = 0; i < maxClients; i++){ //loops through every client thread
+				if(t[i] != null && t[i].clientName != null){ //finds all valid clients
+					t[i].outputStream.writeObject(message);
+					//t[i].outputStream.write(0x00);
+					t[i].outputStream.flush();
+				}
+			}
+		}
+	}
+	
+	public void writeToStream(String message) throws IOException{
+		outputStream.writeObject(new message(message));
+		//outputStream.write(0x00);
+		outputStream.flush();
+	}
+	
+	public String getClientName(){
+		return clientName;
+	}
 
 }
+
